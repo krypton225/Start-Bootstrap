325c116853e1ed4ecc2d0946c59cbead5a4eae4a
krypton225 <k.mohamed.11.98@gmail.com>
2022-07-21 23:59:21 +0200

    ADD Project with its file

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const shrinkNavBar = () => {
+  const mainNavBar = document.getElementById("mainNavBar");
+
+  if (window.scrollY === 0) {
+    mainNavBar.classList.remove("nav-shrink");
+  } else {
+    mainNavBar.classList.add("nav-shrink");
+  }
+};
+
+window.addEventListener("scroll", () => {
+  shrinkNavBar();
+});
+
+const openPhoto = () => {
+  document.querySelectorAll(".card").forEach((e) => {
+    e.addEventListener("click", () => {
+      const getMyPhotoName = e.dataset.photo;
+      const myTitle = document.getElementById("openPhotoModalLabel");
+      const modalBody = document.getElementById("modalBody");
+
+      myTitle.textContent = getMyPhotoName;
+
+      modalBody.innerHTML = `<img src="./assets/images/${getMyPhotoName}.png" class="w-75 d-block text-center m-auto">`;
+    });
+  });
+};
+
+openPhoto();
+
+const closeTooglerAfterClick = () => {
+  document.querySelectorAll(".nav-link").forEach((e) => {
+    e.addEventListener("click", () => {
+      document.getElementById("navLists").classList.remove("show");
+    });
+  });
+};
+
+closeTooglerAfterClick();
